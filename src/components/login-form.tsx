c226aa6f9d6b5ac37e414ab16e514773b1c8c583
Raunak <raunakkumar6437@gmail.com>
2025-08-03 06:50:39 +0530

    fixed issue

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -52,7 +52,7 @@ export function LoginForm({
           <div className="grid gap-6">
             <div className="flex flex-col gap-4">
               <Button
-                disabled={isGithub!}
+                disabled={isGithub}
                 variant="outline"
                 className="w-full"
                 onClick={signwithgithub}
@@ -93,46 +93,6 @@ export function LoginForm({
                 Login with Google
               </Button>
             </div>
-
-            {/* 
-              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                <span className="bg-card text-muted-foreground relative z-10 px-2">
-                  Or continue with
-                </span>
-              </div>
-              
-              <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div> */}
           </div>
         </CardContent>
       </Card>
